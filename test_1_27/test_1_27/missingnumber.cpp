71183824d7ceb3735a89a6365bd884df2ee37f72
zhanghaoyu020418 <zhanghaoyu020418@yeah.net>
2021-01-28 21:53:54 +0800

    likou-1-27

---
+++ test_1_27/test_1_27/missingnumber.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int nums[9] = { 1,2,3,4,5,6,7,8,9 };
+
+	//cout << nums.size() << endl;
+
+	return 0;
+}
