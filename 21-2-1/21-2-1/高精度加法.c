47beed737eeed9d7694377a480d68cbc5961bc3e
zhanghaoyu020418 <zhanghaoyu020418@yeah.net>
2021-02-01 23:45:07 +0800

    21-2-1

---
+++ 21-2-1/21-2-1/高精度加法.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+
+	return 0;
+}
+
+//// C = A + B, A >= 0, B >= 0
+//vector<int> add(vector<int> &A, vector<int> &B)
+//{
+//	if (A.size() < B.size()) return add(B, A);
+//
+//	vector<int> C;
+//	int t = 0;
+//	for (int i = 0; i < A.size(); i++)
+//	{
+//		t += A[i];
+//		if (i < B.size()) t += B[i];
+//		C.push_back(t % 10);
+//		t /= 10;
+//	}
+//
+//	if (t) C.push_back(t);
+//	return C;
+//}
